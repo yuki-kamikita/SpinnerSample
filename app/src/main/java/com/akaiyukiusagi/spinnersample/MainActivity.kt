835888d474ca83a6f646d514867df3ceccecc2d3
shuhei_kiyosawa <s.kiyosawa@frontierfield.co.jp>
2020-12-04 14:12:40 +0900

    現在セットされている値とindexを取得

--- app/src/main/java/com/akaiyukiusagi/spinnersample/MainActivity.kt
+++ app/src/main/java/com/akaiyukiusagi/spinnersample/MainActivity.kt
@@ -2,11 +2,13 @@ package com.akaiyukiusagi.spinnersample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
 import android.widget.ArrayAdapter
-import android.widget.Spinner
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener {
 
     // 選択肢
     private val spinnerItems = arrayOf("ウッディ", "バズ", "ポテトヘッド", "スリンキー", "レックス")
@@ -23,7 +25,30 @@ class MainActivity : AppCompatActivity() {
 
         // AdapterをSpinnerのAdapterとして設定
         spinner.adapter = adapter
-    }
-}
 
+        // onItemSelectedを書いた場所を指定
+        spinner.onItemSelectedListener = this
+
+        // 何番目の値がセットされているか
+        showIndex.setOnClickListener{
+            Toast.makeText(this, spinner.selectedItemPosition.toString(), Toast.LENGTH_SHORT).show()
+        }
+        // セットされている値
+        showValue.setOnClickListener{
+            Toast.makeText(this, spinner.selectedItem.toString(), Toast.LENGTH_SHORT).show()
+        }
+    }
+
+
+    // アイテムが選択された時
+    override fun onItemSelected(parent: AdapterView<*>, view: View, pos: Int, id: Long) {
+        textView.text = spinnerItems[pos]
+    }
+
+    override fun onNothingSelected(parent: AdapterView<*>) {
+        textView.text = "これいつ動くのか不明"
+    }
+
+}
 // 出典：https://hirauchi-genta.com/kotlin-spinner/
+// 公式：https://developer.android.com/guide/topics/ui/controls/spinner?hl=ja
